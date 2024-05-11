b3e15b2edae9fa4e111a7fcffaa82e089cc80d4a
vitorx20i <vitorgabriielm@gmail.com>
2024-05-11 11:57:17 -0300

    Atualização

---
+++ FWA/HTML/Vitor Gabriel/HTML/1º Mão na Massa - JAVASCRIPT/Atividade 5.js
@@ -0,0 +1,8 @@
+/* 5. Multiplique os valores das variáveis numero1 e
+numero2 e guarde o resultado em uma variável
+chamada resultadoMultiplicacao. Depois, mostre o
+resultado no console. */
+let numero1 = 10;
+let numero2 = 20;
+let resultadoMultiplicacao = numero1 * numero2;
+console.log(resultadoMultiplicacao)
