5ab15989f15a4ef0a48baea1d28c62f2436b3fca
Allam Silva <allam.silva@lepavbh.local>
2024-05-11 11:05:10 -0300

    Atividade 1 JS Allam

---
+++ FWA/JS/Allam Gabriel/Mao Na Massa 1/atividade1.js
@@ -0,0 +1,29 @@
+/*
+1-Crie um programa em JavaScript que declare e inicialize variáveis para armazenar os seguintes tipos de dados:
+
+a)Um número inteiro.
+b)Um número de ponto flutuante.
+c)Uma string.
+d)Um booleano.
+e)Um array contendo pelo menos 3 elementos de tipos diferentes.
+f)Um objeto com pelo menos 3 pares de chave-valor, onde as chaves são strings e os valores podem ser de qualquer tipo.
+g)Em seguida, imprima cada uma das variáveis declaradas no console, indicando o tipo de dado que está sendo impresso.
+
+*/
+var a = 1;
+var b = 1.1;
+var c = "Olá Mundo";
+var d = false;
+var e = [1,a,1.1];
+var f = {
+            teste1 : 100,
+            teste2 : "100",
+            teste3 : 100.1
+        }
+console.log("Este tipo de variável é Numérico:" , a);
+console.log("Este tipo de variável é Flutuante:" , b);
+console.log("Este tipo de variável é uma String:" , c);
+console.log("Este tipo de variável é Booleano:" , d);
+console.log("Este tipo de variável é um Array:" , e);
+console.log("Este tipo de variável é um Objeto:" , f);
+
