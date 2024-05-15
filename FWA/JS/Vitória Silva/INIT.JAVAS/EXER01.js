275a0677f7c06c24a52657bb79a046987af2b196
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-05-15 15:47:24 -0300

    mao na massa

---
+++ FWA/JS/Vitória Silva/INIT.JAVAS/EXER01.js
@@ -0,0 +1,15 @@
+let inteiro = 222
+let flutuante = 2.5
+console.log("PERIQUITO")
+console.log(typeof"PERIQUITO")
+let idade = 35
+maior = idade >= 18
+maior = idade < 18
+console.log (idade >18)                                                        
+var bichos = ["tucanito", "periquito", "tico-tico", "galinhita"]
+console.log( typeof bichos[3])
+let frutas = {fruta: "manga", forma: "redonda", epoca: "fevereiro"
+}
+console.log(frutas)
+console.log(typeof frutas)
+
