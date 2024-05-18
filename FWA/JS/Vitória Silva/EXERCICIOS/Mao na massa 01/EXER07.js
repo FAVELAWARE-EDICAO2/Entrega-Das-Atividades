64f839d0c3a434111a08fc57c29092da0f11c65f
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-05-18 11:54:10 -0300

    JMNM

---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 01/EXER07.js
@@ -0,0 +1,4 @@
+let numero1 = 32
+let numero2 = 8
+resultadoSubtracao = (numero1 - numero2)
+console.log (resultadoSubtracao)
---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 02/EXER03.js
@@ -0,0 +1,5 @@
+let idade = 43
+if( idade =< 12){
+ console.log("Criança");
+}
+else()
