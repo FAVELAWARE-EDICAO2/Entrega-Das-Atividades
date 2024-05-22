0ca9c517432f3e716ce2d4ce740baf2b50d56dbe
Kethelyn-Sarah <1234.5678skaa@gmail.com>
2024-05-22 13:41:26 -0300

    arquivo js

--- FWA/JS/Sarah_K/mao2/atv2.js
+++ FWA/JS/Sarah_K/mao2/atv2.js
@@ -1,1 +1,9 @@
- 
+let num = 2;
+if (num > 0) {
+  console.log(` ${num} é um numero positivo`);
+} else if (num == 0) {
+  console.log("Seu numero é zero");
+}
+else {
+    console.log ("Seu numero é negativo")
+}
