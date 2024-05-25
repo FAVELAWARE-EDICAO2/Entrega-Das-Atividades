8817df5a6357f1713da6635792ac88c0c833c8ff
lukas17alves <lukas17alvesfreitas@gmail.com>
2024-05-25 13:32:44 -0300

    Subindo mão na massa 1 e 2

---
+++ FWB/JS/Lucas Alves/exercicio01/atividademaonamasa11.js
@@ -0,0 +1,9 @@
+// Atribuição do seu nome à variável nome
+let nome = "Lucas";
+
+// Criação da mensagem de saudação com o nome e atribuição à variável mensagem
+let mensagem = "Olá, meu nome é " + nome + "! Bem-vindo!";
+
+// Exibição da mensagem no console
+console.log(mensagem);
+
---
+++ FWB/JS/Lucas Alves/exercicio01/atividademaonamasa7.js
@@ -0,0 +1,10 @@
+// Declaração e inicialização das variáveis numero1 e numero2
+let numero1 = 10;
+let numero2 = 10;
+
+// Subtração do valor de numero2 do valor de numero1 e armazenamento do resultado em uma variável chamada resultadoSubtracao
+let resultadoSubtracao = numero1 - numero2;
+
+// Impressão do resultado no console
+console.log("Resultado da subtração:", resultadoSubtracao);
+
