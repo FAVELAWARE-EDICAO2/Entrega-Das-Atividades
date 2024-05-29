15a43056227b0312e8193a69b409535def585c4f
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-05-29 15:29:16 -0300

    JMNM9/10

---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 02/EXER08.js
@@ -0,0 +1,18 @@
+let num1  = parseInt(prompt("Insira sua nota:"))
+switch (num1) {
+    case   9 |   10:
+        console.log("A");
+    break;
+    case   7 |   8:
+        console.log("B");
+    break;
+    case   5 |   6:
+        console.log("C");
+    break;
+    case   3 |   4:
+        console.log("D");
+    break;
+    case   0 |   2:
+        console.log("F");
+    break;
+}
