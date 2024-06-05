6478fe8b94e4528ca1c039b189ae48c76e8434a2
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-05 16:00:08 -0300

    MNMJS3

---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER01.js
@@ -0,0 +1,49 @@
+// let num = [9,8,7,6,5,"bolo",3,2,1, "oi"]
+// console.log(num.length, "valor") //Mostra o tamanho do array e quantidade de coisas que tem dentro.
+
+// for(let i = 0; i<= num.length; i++){   //daqui conta do xero ate o 9 que é a quantidade de coisas dentro do array
+// console.log(i)
+// }
+// console.log("------")
+// for(let element of num){
+//     console.log(element)
+
+// }
+
+let num3 = 5
+for(let n = 2; n < num3; n++ ){
+    conta = num3 % n 
+    console.log(conta)
+    if(num3 %  n == 0){
+
+        console.log("é primo")
+
+
+    }
+}
+
+
+  var numero = 50;
+  var divisores= 0;
+
+  for(var count=1 ; count<=numero ; count++)
+   if(numero % count == 0){
+
+       divisores++;
+   }
+
+
+
+
+
+
+
+
+    // for(const element of num){
+// console.log(element,"valor")
+
+//     for(let i = 2;i <= element;i++){
+//     console.log (i)
+// }
+// console.log("----")
+// }
