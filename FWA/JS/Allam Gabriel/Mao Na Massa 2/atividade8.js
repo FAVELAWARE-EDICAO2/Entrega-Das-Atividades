4b069fa8c21377199cb6d942bc4ce2cfa921864a
Allam Gabriel <allamaprendiz@gmail.com>
2024-06-08 09:36:34 -0300

    Entrega Completa JS Allam

--- FWA/JS/Allam Gabriel/Mao Na Massa 2/atividade8.js
+++ FWA/JS/Allam Gabriel/Mao Na Massa 2/atividade8.js
@@ -7,3 +7,36 @@ D: 3-4
 F: 0-2
 Se a nota estiver fora do intervalo de 0 a 10, imprima "Nota inválida".
 */
+let nOta = 6
+
+
+switch (nOta){
+    case 1:
+    console.log("F")
+    break
+    case 2:
+    console.log("F")
+    case 3:
+    console.log("D")
+    break
+    case 4:
+    console.log("D")
+    break
+    case 5:
+    console.log("C")
+    case 6:
+    console.log("C")
+    break
+    case 7:
+    console.log("B")
+    break
+    case 8:
+    console.log("B")
+    break
+    case 9:
+    console.log("A")
+    break
+    case 10:
+    console.log("A")
+    break
+}
