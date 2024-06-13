92458b41d401588bc1d741a0b3bdffee993da3d0
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-13 15:21:58 -0300

    MNMJS

--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER01.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER01.js
@@ -1,49 +1,15 @@
-// let num = [9,8,7,6,5,"bolo",3,2,1, "oi"]
-// console.log(num.length, "valor") //Mostra o tamanho do array e quantidade de coisas que tem dentro.
+let numeros = [9,8,7,6,5,4,3,2,1] // declaração do array
+let contador = 0
+for(i = 0; i < numeros.length; i++){ // peercorrendo o array e pegando sseu indices começando do 0 ate o 8
 
-// for(let i = 0; i<= num.length; i++){   //daqui conta do xero ate o 9 que é a quantidade de coisas dentro do array
-// console.log(i)
-// }
-// console.log("------")
-// for(let element of num){
-//     console.log(element)
-
-// }
-
-let num3 = 5
-for(let n = 2; n < num3; n++ ){
-    conta = num3 % n 
-    console.log(conta)
-    if(num3 %  n == 0){
-
-        console.log("é primo")
-
-
-    }
+    for(j = 1; j <= numeros[i]; j++){ //percorre ate o numero do indice e faz o modulo  pra verificar se o resto é igual a zero 
+       if(numeros[i] % j == 0){
+        contador++ 
+       }
+ }
+if(contador == 2){
+    console.log(`${numeros[i]} sua posição é:${i}`)
 }
+    contador = 0 //zerando o contador pra que ele possa repetir vazio e guardar apenas dois valores novamente
 
-
-  var numero = 50;
-  var divisores= 0;
-
-  for(var count=1 ; count<=numero ; count++)
-   if(numero % count == 0){
-
-       divisores++;
-   }
-
-
-
-
-
-
-
-
-    // for(const element of num){
-// console.log(element,"valor")
-
-//     for(let i = 2;i <= element;i++){
-//     console.log (i)
-// }
-// console.log("----")
-// }
+}
