06291bf0365ea76c25a388f12046b54e4813c5be
Maristela Dias <maristela.dias@lepavbh.local>
2024-06-13 15:51:19 -0300

    mao na masa 3

---
+++ FWA/JS/Maristela/mao na massa 2/mao na massa 3/atv2.js
@@ -0,0 +1,123 @@
+tinderdosSonhos
+
+ ("Bem-vindo ao Tinder dos Sonhos!");
+
+ ("Digite a altura da pessoa (de 0 a 10): ");
+ altura = scanner.nextInt();
+
+ ("Digite a beleza da pessoa (de 0 a 10): ");
+beleza = scanner.nextInt();
+
+ ("Digite o carisma da pessoa (de 0 a 10): ");
+carisma = scanner.nextInt();
+
+media = (altura + beleza + carisma) / 3.0;
+
+        if (altura >= 7 && beleza >= 7 && carisma >= 7) {
+            System.out.println("Booooraa! Essa pessoa é perfeita para você!");
+        } else if (media >= 6.0) {
+            System.out.println("Essa pessoa tem potencial! Vale a pena dar uma chance.");
+        } else 
+            System.out.println("Continue buscando... O amor dos seus sonhos ainda está por aí!");
+
+            ("Bem-vindo ao Tinder dos Sonhos!");
+
+    ("Digite a altura da pessoa (de 0 a 10): ");
+     altura = scanner.nextInt();
+   
+   ("Digite a beleza da pessoa (de 0 a 10): ");
+    beleza = scanner.nextInt();   
+
+    ("Digite o carisma da pessoa (de 0 a 10): ");
+    carisma = scanner.nextInt(); 
+
+     notasAcimaDe7 = 0;
+        if (altura >= 7) {
+            notasAcimaDe7++;
+        }
+        if (beleza >= 7) {
+            notasAcimaDe7++;
+        }
+        if (carisma >= 7) {
+            notasAcimaDe7++;
+        }
+
+        if (notasAcimaDe7 >= 2) {
+            System.out.println("Só vai! Essa pessoa é interessante.");
+        } else if (media >= 6.0) {
+            System.out.println("Essa pessoa tem potencial! Vale a pena dar uma chance.");
+        } else {
+            System.out.println("Continue buscando... O amor dos seus sonhos ainda está por aí!");
+        }
+
+
+
+
+        ("Bem-vindo ao Tinder dos Sonhos!"); 
+     
+        ("Digite a altura da pessoa (de 0 a 10): ");
+         altura = scanner.nextInt();
+         
+         ("Digite a beleza da pessoa (de 0 a 10): ");
+         beleza = scanner.nextInt(); 
+
+         ("Digite o carisma da pessoa (de 0 a 10): ");
+         carisma = scanner.nextInt();
+
+          notasAcimaDe7 = 0;
+        if (altura >= 7) {
+            notasAcimaDe7++;
+        }
+        if (beleza >= 7) {
+            notasAcimaDe7++;
+        }
+        if (carisma >= 7) {
+            notasAcimaDe7++;
+        }
+
+        if (notasAcimaDe7 == 1) {
+            System.out.println("Fé em Deus... Essa pessoa pode ser a certa!");
+        } else if (notasAcimaDe7 >= 2) {
+            System.out.println("Só vai! Essa pessoa é interessante.");
+        } else if (media >= 6.0) {
+            System.out.println("Essa pessoa tem potencial! Vale a pena dar uma chance.");
+        } else 
+            System.out.println("Continue buscando... O amor dos seus sonhos ainda está por aí!");
+
+
+
+
+
+            ("Bem-vindo ao Tinder dos Sonhos!");
+            
+            ("Digite a altura da pessoa (de 0 a 10): ");
+            altura = scanner.nextInt();   
+
+            ("Digite a beleza da pessoa (de 0 a 10): ");
+            beleza = scanner.nextInt();
+
+            ("Digite o carisma da pessoa (de 0 a 10): ");
+            carisma = scanner.nextInt(); 
+
+             notasAcimaDe7 = 0;
+            if (altura >= 7) {
+                notasAcimaDe7++;
+            }
+            if (beleza >= 7) {
+                notasAcimaDe7++;
+            }
+            if (carisma >= 7) {
+                notasAcimaDe7++;
+            }
+    
+            if (notasAcimaDe7 == 0) {
+                System.out.println("Misericórdia! Essa pessoa não atende aos seus requisitos.");
+            } else if (notasAcimaDe7 == 1) {
+                System.out.println("Fé em Deus... Essa pessoa pode ser a certa!");
+            } else if (notasAcimaDe7 >= 2) {
+                System.out.println("Só vai! Essa pessoa é interessante.");
+            } else if (media >= 6.0)
+                System.out.println("Essa pessoa tem potencial! Vale a pena dar uma chance.");
+            
+     
+  
