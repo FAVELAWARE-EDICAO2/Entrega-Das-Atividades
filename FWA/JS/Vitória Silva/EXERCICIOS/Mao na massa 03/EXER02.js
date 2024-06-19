2b53b9ef38a0ec8490a8cbe66d7c7eb020ce5fa7
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-19 15:26:08 -0300

    ...

---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER02.js
@@ -0,0 +1,13 @@
+let altura =parseInt(prompt("Qual a nota para a altura?"))
+let beleza = parseInt(prompt("Qual a nota para a beleza?"))
+ let carisma = parseInt(prompt("Qual a nota para a carisma?"))
+ switch (beleza, altura,carisma) {
+    case beleza & altura & carisma > 7:
+        console.log("Booooraa trem!")
+        break;
+        case beleza & carisma > 7 | carisma & altura > 7 | beleza & altura:
+console.log(" É o que tem, so vai...")
+        break;
+        case :
+        
+ }
