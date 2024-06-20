53e26fcce90ef97db5ce869a9072d166d8f89edb
João Pedro Rosa <joao.rosa@lepavbh.local>
2024-06-20 14:20:35 -0300

    cristiano ronaldo

---
+++ FWA/JS/Joao Pedro/mao na massa/atv11.js
@@ -0,0 +1,5 @@
+// Criação da mensagem de saudação com meu nome
+var mensagem = "Olá! Meu nome é Joao Pedro. Como posso ajudar você hoje?";
+
+// Exibição da mensagem no console
+console.log(mensagem);
---
+++ FWA/JS/Joao Pedro/mao na massa/atv2.js
@@ -0,0 +1,10 @@
+// Declaração das variáveis
+var nome = "João";
+var idade = 25;
+var cidade = "São Paulo";
+
+// Concatenação de strings para criar a mensagem de saudação
+var mensagem = "Olá, meu nome é " + nome + ", tenho " + idade + " anos e moro em " + cidade + ".";
+
+// Impressão da mensagem no console
+console.log(mensagem);
