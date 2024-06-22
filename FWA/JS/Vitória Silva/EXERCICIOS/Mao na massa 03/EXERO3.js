036bd817d4a95e09f674ffe6750b3bcef4787df5
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-22 11:31:47 -0300

    MNMJS

--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
@@ -3,3 +3,11 @@ for (i = 0; i < 5; i++) {
   numeros[i] =  parseInt(prompt("insira um numero"));
   console.log(numeros[i])
 }
+let soma;
+for(i = 0;i<5;i++){
+  soma = numeros[i]+=numeros[i]
+  
+}
+
+console.log(soma)
+// for(){}
