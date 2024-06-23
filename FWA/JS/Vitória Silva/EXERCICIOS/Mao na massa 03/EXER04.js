8555cb521645dff19663b969523776d6429e477e
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-23 16:02:58 -0300

    MNMJS

---
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER04.js
@@ -0,0 +1,7 @@
+let numeros = [3, 5, 7, 9, 11];
+console.log(numeros)
+numeros.push(13)
+console.log(numeros)
+numeros.shift()
+console.log(numeros)
+console.log(numeros[2])
--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
@@ -1,13 +1,11 @@
-let numeros = [];
-for (i = 0; i < 5; i++) {
+let numeros = [];// Declarei um array vazio
+for (i = 0; i < 5; i++) { //fiz um for pra preencher o array
   numeros[i] =  parseInt(prompt("insira um numero"));
   console.log(numeros[i])
 }
-let soma;
-for(i = 0;i<5;i++){
+let soma;// Declaração de variavel 
+for(i = 0;i<5;i++){//outro for pra somar 
   soma = numeros[i]+=numeros[i]
-  
-}
+} //Este for 
 
 console.log(soma)
-// for(){}
