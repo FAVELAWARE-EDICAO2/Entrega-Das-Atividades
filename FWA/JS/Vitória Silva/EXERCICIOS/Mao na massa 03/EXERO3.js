eb7a841aef6d65f01c8702e2ec97f65eccd0240d
Vhickyzambi <151404410+Vhickyzambi@users.noreply.github.com>
2024-06-26 15:31:54 -0300

    Update EXERO3.js

--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXERO3.js
@@ -3,9 +3,11 @@ for (i = 0; i < 5; i++) { //fiz um for pra preencher o array
   numeros[i] =  parseInt(prompt("insira um numero"));
   console.log(numeros[i])
 }
-let soma;// Declaração de variavel 
-for(i = 0;i<5;i++){//outro for pra somar 
-  soma = numeros[i]+=numeros[i]
-} //Este for 
+let soma;
+for(i = 0; i <5;i++){
+  soma += numeros[i]//i = 0 / soma = 0/
+  //soma = soma + numeros[i] / 0 = 0 + 1   
+  
+}
 
 console.log(soma)
