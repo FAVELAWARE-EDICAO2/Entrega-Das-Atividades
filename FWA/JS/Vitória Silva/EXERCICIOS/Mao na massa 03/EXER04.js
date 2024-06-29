205c0993724725779db49bcff5220d3f1cc3eb7e
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-29 11:51:46 -0300

    MNMJS

--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER04.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER04.js
@@ -1,4 +1,5 @@
 let numeros = [3, 5, 7, 9, 11];
+let pinguim = [9,4,7,6,8,4,7]
 console.log(numeros)
 numeros.push(13)
 console.log(numeros)
@@ -6,4 +7,17 @@ numeros.shift()
 console.log(numeros)
 console.log(numeros.indexOf(9))
 console.log(numeros.sort((a,b) => b - a))
-console.log(numeros.sort((a, b) => a - b))
+console.log(numeros.reverse())
+function maiorQueSete(numeros){
+    return numeros > 7;
+}
+console.log(numeros.filter(maiorQueSete))
+
+let soma = 0;
+for(let i = 0; i <numeros.length;i++){
+  soma += numeros[i]
+}
+console.log(Number(soma))
+
+console.log(numeros)
+console.log(soma)
