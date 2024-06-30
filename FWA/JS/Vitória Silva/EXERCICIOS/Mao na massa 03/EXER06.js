d8a156637d8ae944ea9d7bf2cf00f02c950a5bde
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-06-30 11:36:51 -0300

    MNMJS

--- FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER06.js
+++ FWA/JS/Vitória Silva/EXERCICIOS/Mao na massa 03/EXER06.js
@@ -1,1 +1,9 @@
+
+// 6. Dado um array de números inteiros, realize as seguintes operações:
+
+// Remova todos os números pares do array.
+// Multiplique cada número ímpar por 2.
+// Ordene os números resultantes em ordem decrescente.
+// Mostre o array resultante após essas operações.
+
 let numbers= [1,2,6,9,7]
