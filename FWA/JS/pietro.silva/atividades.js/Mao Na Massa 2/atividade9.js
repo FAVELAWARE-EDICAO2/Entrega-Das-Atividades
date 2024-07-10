4309c59b219e388b36b750f1f22601046805cbc8
pietronet <pietro.silva@lepavbh.local>
2024-07-10 13:54:22 -0300

    atividaes js

---
+++ FWA/JS/pietro.silva/atividades.js/Mao Na Massa 2/atividade9.js
@@ -0,0 +1,13 @@
+let  cor ="vermelho"
+
+switch (cor){
+    case "vermelho":
+    console.log("PARE!!")
+    break
+    case "amarelo":
+    console.log("ATENÇÃO!!")
+    break
+    case "verde":
+    console.log("LIVRE!")
+
+}
